f6a1b1cb34b351c3830619dce034a4d4ef7c1b3a
Baychik <ergeshovbaistan@gmail.com>
2023-10-16 17:52:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw_1_4/ui/profile/ProfileFragment.kt
@@ -0,0 +1,32 @@
+package com.example.hw_1_4.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.hw_1_4.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private var _binding:FragmentProfileBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
